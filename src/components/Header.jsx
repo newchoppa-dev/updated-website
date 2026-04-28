8c1cea29b57f78172a5ba9f1e6ccee2aef4245c3
Daniil <freddypink22@gmail.com>
2026-04-28 20:02:48 +0300

    updated website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ export default function Header({ t }) {
   return (
     <header>
       <div className="container layout">
-        <img className="borderline" src="/images/1.png" alt="" />
+        <img className="borderline" src="images/1.png" alt="" />
 
         <h1 className="title">{t.main_title}</h1>
 
-        <img src="/images/title.gif" alt="" />
+        <img src="images/title.gif" alt="" />
 
         <p className="title">
           <strong>{t.portfolio_title}</strong>
